0556bd4e9c0f6469cd129b486697a6bf8e868388
serewann <serewann@gmail.com>
2016-09-16 18:36:10 +0300

    Add permission check and clear screen before start

--- src/fast-adminer.php
+++ src/fast-adminer.php
@@ -1,11 +1,21 @@
 <?php
 
+
+// Clear screen
+if (PHP_OS == 'Linux') {
+    system('clear');
+} else {
+    system('cls');
+}
+
 echo "Downloading last Adminer version... (~400 KB) ";
 
 if (!file_exists('./adminer.php')) {
-    file_put_contents('./adminer.php',
+    if (!file_put_contents('./adminer.php',
         file_get_contents('http://www.adminer.org/latest.php')
-    );
+    )) {
+        echo "Can`t write file to the disk: use sudo.";
+    };
 } else {
     die("\n \nError: file adminer.php already exists in \n" . __DIR__ . "\n");
 }
@@ -15,7 +25,7 @@ echo "Downloaded! \n \n";
 echo "Now you can access Adminer under this URL: \n";
 echo "\e[4m\033[1mhttp://\e[0m\e[4m[YOUR-SITE-URL]\e[4m\e[1m/adminer.php \e[0m \n \n";
 
-readline("Press \e[1mEnter\e[0m to remove Adminer file and return to the terminal. \n \n");
+readline("Press Enter to remove Adminer file and return to the terminal. \n \n");
 
 if (unlink('./adminer.php')) {
     echo "Adminer.php was removed...";
