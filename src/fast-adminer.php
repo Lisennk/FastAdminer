0daf69ef8d7ce01b82de33625414a7ec21958d85
serewann <serewann@gmail.com>
2016-09-18 14:30:52 +0300

    Remove windows support and make some refactoring

--- src/fast-adminer.php
+++ src/fast-adminer.php
@@ -1,36 +1,40 @@
 <?php
 
+try {
+    if (PHP_OS != 'Linux') throw new Exception('This script supports only Linux');
+    if (file_exists('./adminer.php')) throw new Exception('file adminer.php already exists');
 
-// Clear screen
-if (PHP_OS == 'Linux') {
-    system('clear');
-} else {
-    system('cls');
+    echo "Downloading last Adminer version... (~400 KB) ";
+
+    if (!file_put_contents('./adminer.php', file_get_contents('http://www.adminer.org/latest.php')))
+        throw new Exception('Can`t write file to the disk, try to use sudo');
+
+
+    echo "Downloaded! \n \n"
+       . "Now you can access Adminer under this URL: \n";
+
+    echo "\e[4m" // <underline>
+        . "\e[1m" // <bold>
+
+        . "http://"
+
+        . "\e[0m" // </bold> </underline>
+        . "\e[4m" // <underline>
+
+        . "[YOUR-SITE-URL]"
+
+        . "\e[1m" // <bold>
+
+        . "/adminer.php"
+
+        . "\e[0m" // </bold> </underline>
+        . "\n \n";
+
+    readline("Press Enter to remove Adminer file and return to the terminal. \n \n");
+
+    if (!unlink('./adminer.php')) throw new Exception("Can`t remove adminer.php");
+
+    echo "Adminer was removed... \n";
+} catch (Exception $e) {
+    echo "\n Error: ". $e->getMessage() . "\n";
 }
-
-echo "Downloading last Adminer version... (~400 KB) ";
-
-if (!file_exists('./adminer.php')) {
-    if (!file_put_contents('./adminer.php',
-        file_get_contents('http://www.adminer.org/latest.php')
-    )) {
-        echo "Can`t write file to the disk: use sudo.";
-    };
-} else {
-    die("\n \nError: file adminer.php already exists in \n" . __DIR__ . "\n");
-}
-
-echo "Downloaded! \n \n";
-
-echo "Now you can access Adminer under this URL: \n";
-echo "\e[4m\033[1mhttp://\e[0m\e[4m[YOUR-SITE-URL]\e[4m\e[1m/adminer.php \e[0m \n \n";
-
-readline("Press Enter to remove Adminer file and return to the terminal. \n \n");
-
-if (unlink('./adminer.php')) {
-    echo "Adminer.php was removed...";
-} else {
-    echo "Error: Can`t remove adminer.php :(";
-}
-
-echo "\n";
